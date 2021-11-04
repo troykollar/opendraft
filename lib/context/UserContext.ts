2df29317c06ea8a6be3195732f9e124476693e9f
Troy Kollar <troykollar@gmail.com>
2021-11-03 23:28:47 -0400

    Remove userData from UserContext to prevent issues

--- lib/context/UserContext.ts
+++ lib/context/UserContext.ts
@@ -4,5 +4,4 @@ import { useDocumentData } from "react-firebase-hooks/firestore";
 
 export const UserContext = createContext({
   user: null as User | null | undefined,
-  userData: null as ReturnType<typeof useDocumentData> | null,
 });
