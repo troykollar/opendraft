d25c0e9dfb00fcd520969cf4dcf113563465ef58
Troy Kollar <troykollar@gmail.com>
2021-11-03 23:29:32 -0400

    Add FullPageLoader when auth state is loading

---
+++ components/FullPageLoader.tsx
@@ -0,0 +1,22 @@
+import { FunctionComponent } from "react";
+import { CircularProgress, Container } from "@mui/material";
+
+interface FullPageLoaderProps {}
+
+const FullPageLoader: FunctionComponent<FullPageLoaderProps> = () => {
+  return (
+    <Container
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <CircularProgress />
+    </Container>
+  );
+};
+
+export default FullPageLoader;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,28 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { useContext } from "react";
 import { UserContext } from "lib/context/UserContext";
+import { useAuthState } from "react-firebase-hooks/auth";
 import Login from "components/Login/Login";
+import { auth } from "lib/firebase/auth";
+import { Button } from "@mui/material";
+import { signOut } from "firebase/auth";
+import FullPageLoader from "components/FullPageLoader";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const { user, userData } = useContext(UserContext);
+  const [user, loading, error] = useAuthState(auth);
   return (
-    <UserContext.Provider value={{ user, userData }}>
-      {user ? <Component {...pageProps} /> : <Login />}
+    <UserContext.Provider value={{ user }}>
+      <div>{JSON.stringify(user)}</div>
+      {user ? (
+        <>
+          <Button onClick={() => signOut(auth)}>Sign Out</Button>
+          <Component {...pageProps} />
+        </>
+      ) : loading ? (
+        <FullPageLoader />
+      ) : (
+        <Login />
+      )}
     </UserContext.Provider>
   );
 }
