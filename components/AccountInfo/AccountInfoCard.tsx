b67a70b0e489e8c2035fb3b379248d3bf45d283d
Troy Kollar <troykollar@gmail.com>
2021-11-05 20:31:26 -0400

    Add PwdChange Dialog to AccountInfoCard

--- components/AccountInfo/AccountInfoCard.tsx
+++ components/AccountInfo/AccountInfoCard.tsx
@@ -1,5 +1,5 @@
 import { User } from "firebase/auth";
-import { FunctionComponent, useContext } from "react";
+import { FunctionComponent, useContext, useState } from "react";
 import { UserContext } from "lib/context/UserContext";
 import {
   Container,
@@ -12,70 +12,84 @@ import {
   Button,
 } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
+import PwdChangeDialog from "components/AccountInfo/PwdChangeDialog";
 
 interface AccountInfoCardProps {}
 
 const AccountInfoCard: FunctionComponent<AccountInfoCardProps> = () => {
   const { user, userData } = useContext(UserContext);
+  const [showChangePassword, setShowChangePassword] = useState(false);
+
   return (
-    <Container maxWidth="sm">
-      <Grid container>
-        <Grid item xs={12}>
-          <Grid container>
-            <Grid item xs={12}>
-              <Typography variant="h6" sx={{ fontWeight: "bold" }}>
-                Email
-              </Typography>
+    <>
+      <Container maxWidth="sm">
+        <Grid container>
+          <Grid item xs={12}>
+            <Grid container>
+              <Grid item xs={12}>
+                <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                  Email
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sx={{ wordWrap: "break-word" }}>
+                <Typography variant="h6">{user && user.email}</Typography>
+              </Grid>
             </Grid>
-            <Grid item xs={12} sx={{ wordWrap: "break-word" }}>
-              <Typography variant="h6">{user && user.email}</Typography>
+            <Divider sx={{ margin: "16px 0px 16px 0px" }} />
+            <Grid container>
+              <Grid item xs={12}>
+                <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                  Username
+                </Typography>
+              </Grid>
+              <Grid item xs={8} sx={{ wordWrap: "break-word" }}>
+                <Typography variant="h6">
+                  {userData && userData.username}
+                </Typography>
+              </Grid>
+              <Grid item xs={4} sx={{ textAlign: "center" }}>
+                <IconButton>
+                  <EditIcon />
+                </IconButton>
+              </Grid>
             </Grid>
-          </Grid>
-          <Divider sx={{ margin: "16px 0px 16px 0px" }} />
-          <Grid container>
-            <Grid item xs={12}>
-              <Typography variant="h6" sx={{ fontWeight: "bold" }}>
-                Username
-              </Typography>
+            <Divider sx={{ margin: "16px 0px 16px 0px" }} />
+            <Grid container>
+              <Grid item xs={8}>
+                <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                  Password
+                </Typography>
+              </Grid>
+              <Grid item xs={4} sx={{ textAlign: "center" }}>
+                <Button
+                  onClick={() => setShowChangePassword(true)}
+                  variant="contained"
+                >
+                  Change
+                </Button>
+              </Grid>
             </Grid>
-            <Grid item xs={8} sx={{ wordWrap: "break-word" }}>
-              <Typography variant="h6">
-                {userData && userData.username}
-              </Typography>
-            </Grid>
-            <Grid item xs={4} sx={{ textAlign: "center" }}>
-              <IconButton>
-                <EditIcon />
-              </IconButton>
-            </Grid>
-          </Grid>
-          <Divider sx={{ margin: "16px 0px 16px 0px" }} />
-          <Grid container>
-            <Grid item xs={8}>
-              <Typography variant="h6" sx={{ fontWeight: "bold" }}>
-                Password
-              </Typography>
-            </Grid>
-            <Grid item xs={4} sx={{ textAlign: "center" }}>
-              <Button variant="contained">Change</Button>
-            </Grid>
-          </Grid>
-          <Divider sx={{ margin: "16px 0px 16px 0px" }} />
-          <Grid container>
-            <Grid item xs={8}>
-              <Typography variant="h6" sx={{ fontWeight: "bold" }}>
-                Delete Account
-              </Typography>
-            </Grid>
-            <Grid item xs={4} sx={{ textAlign: "center" }}>
-              <Button variant="contained" color="error">
-                Delete
-              </Button>
+            <Divider sx={{ margin: "16px 0px 16px 0px" }} />
+            <Grid container>
+              <Grid item xs={8}>
+                <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                  Delete Account
+                </Typography>
+              </Grid>
+              <Grid item xs={4} sx={{ textAlign: "center" }}>
+                <Button variant="contained" color="error">
+                  Delete
+                </Button>
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+      <PwdChangeDialog
+        open={showChangePassword}
+        handleClose={() => setShowChangePassword(false)}
+      />
+    </>
   );
 };
 
